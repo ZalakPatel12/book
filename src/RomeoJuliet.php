d7d3a49845b8f05202be0e1766ea605058125a0b
Angela Arroyo <ara55@njit.edu>
2019-03-12 23:45:24 -0400

    created RomeoJuliet.php and wrote functions for it.

--- src/RomeoJuliet.php
+++ src/RomeoJuliet.php
@@ -5,3 +5,25 @@
  * Date: 3/12/2019
  * Time: 11:35 PM
  */
+
+class RomeoJuliet extends book
+{
+    public function BookGenre(String $input): String
+    {
+        if (strpos($input, "Comedy")) {
+            echo("This book is a comedy");
+        } else if (strpos($input, "Tragedy")) {
+            echo("This Shoe is a tragedy");
+        }
+    }
+    public function Original Language(String $input): String
+    {
+        if (strpos($input, "English")) {
+            echo("This book was originally written in English);
+        }
+
+        else if (strpos($input, "Latin")) {
+            echo("This was originally written in Latin");
+        }
+    }
+}
